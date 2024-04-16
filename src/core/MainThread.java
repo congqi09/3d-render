af44170bfc5022ed6ef5b6b80b0d3d134cb78896
Cong Qi <qi.co@northeastern.edu>
2024-04-15 22:20:25 -0700

    using arraycopy set background color

--- src/core/MainThread.java
+++ src/core/MainThread.java
@@ -6,12 +6,15 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferInt;
 
+import static java.lang.Math.min;
+
 public class MainThread extends JFrame {
 
     public static int screen_w = 1024;
     public static int screen_h = 682;
     public static int half_screen_w = screen_w / 2;
     public static int half_screen_h = screen_h / 2;
+    public static int screenSize = screen_w * screen_h;
 
     public static JPanel panel;
     public static int[] screen;
@@ -49,20 +52,9 @@ public class MainThread extends JFrame {
 
         while (true) {
 
-            int r_skyblue = 163, g_skyblue = 216, b_skyblue = 239;
-            int r_orange = 255, g_orange = 128, b_orange = 0;
-
-            // rolling skyblue to orange
-            for (int i = 0; i < screen_w; i++) {
-                int p = (i+frameIndex*8) % screen_w;
-                float t1 = Math.abs((float) (half_screen_w - p)/half_screen_w);
-                float t2 = 1f - t1;
-                int r = (int)(r_orange*t1 + r_skyblue*t2);
-                int g = (int)(g_orange*t1 + g_skyblue*t2);
-                int b = (int)(b_orange*t1 + b_skyblue*t2);
-                for (int j = 0; j < screen_h; j++) {
-                    screen[i+j*screen_w] = (r << 16) | (g << 8) | b;
-                }
+            screen[0] = (163 << 16) | (216 << 8) | 239; // skyblue
+            for (int i = 1; i < screen.length; i<<=1) {
+                System.arraycopy(screen, 0, screen, i, min(i, screenSize - i));
             }
 
             frameIndex++;
